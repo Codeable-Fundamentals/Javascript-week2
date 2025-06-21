662bc7593ac2e4aab006352f018542cb5d8ec81c
Grecia Delgado <greciadm2596@gmail.com>
2025-06-20 20:52:56 -0500

    Add perfomance

--- condicionales/ejemplo2.js
+++ condicionales/ejemplo2.js
@@ -1,19 +1,24 @@
 let tipo_de_comida = "marisco";
 
-// if (tipo_de_comida === "china") {
-//   console.log("Vamos al chifa!");
-// } else if (tipo_de_comida === "pollo ala brasa") {
-//   console.log("Vamos a la polleria ROCKYS!");
-// } else if (tipo_de_comida === "pollo") {
-//   console.log("Vamos a la pollada!");
-// } else if (tipo_de_comida === "mariscos") {
-//   console.log("Vamos a la cevicheria");
-// } else if (tipo_de_comida === "arepa") {
-//   console.log("Vamos a la areperia");
-// } else {
-//   console.log("Hoy no quedamos en casa , por que ahi hay comida :c ");
-// }
+const inicio = performance.now();
 
+if (tipo_de_comida === "china") {
+  console.log("Vamos al chifa!");
+} else if (tipo_de_comida === "pollo ala brasa") {
+  console.log("Vamos a la polleria ROCKYS!");
+} else if (tipo_de_comida === "pollo") {
+  console.log("Vamos a la pollada!");
+} else if (tipo_de_comida === "mariscos") {
+  console.log("Vamos a la cevicheria");
+} else if (tipo_de_comida === "arepa") {
+  console.log("Vamos a la areperia");
+} else {
+  console.log("Hoy no quedamos en casa , por que ahi hay comida :c ");
+}
+const fin = performance.now();
+console.log("Usando el Else if : ", fin - inicio);
+
+const inicio2 = performance.now();
 // sea mejor el uso de swich case
 switch (tipo_de_comida) {
   case "china":
@@ -28,6 +33,12 @@ switch (tipo_de_comida) {
   case "mariscos":
     console.log("Vamos a la cevicheria");
     break;
+  case "arepa":
+    console.log("Vamos a la areperia");
+    break;
   default:
     console.log("Hoy no quedamos en casa , por que ahi hay comida :c");
 }
+const fin2 = performance.now();
+
+console.log("Usando el Switch : ", fin2 - inicio2);
---
+++ condicionales/ejemplo3.js
@@ -0,0 +1,3 @@
+let llueve = true;
+let mensaje = llueve ? "¡Lleva un paraguas!" : "No necesitas un paraguas.";
+console.log(mensaje);
