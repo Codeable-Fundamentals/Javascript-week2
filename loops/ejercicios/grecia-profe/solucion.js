bb6515f79c9c7103cd9c70fb798c483217a57ab7
Grecia Delgado <greciadm2596@gmail.com>
2025-06-24 18:58:17 -0600

    add ejercicio

--- loops/ejercicios/grecia-profe/solucion.js
+++ loops/ejercicios/grecia-profe/solucion.js
@@ -1,14 +1,13 @@
 // Ejercicio: Contar vocales en una palabra usando FOR
 // Pedir palabra al usuario y contar cuántas vocales tiene
 // Vocales: a, e, i, o, u (considerar mayúsculas y minúsculas)
-//aqui implementacion
-const prompt = require("prompt-sync")();
+const promt = require("prompt-sync")();
 
-const word = prompt("Escribe una palabra: ");
+const word = promt("Escribe una palabra: ");
 
 let counter = 0;
 for (let i = 0; i < word.length; i++) {
-  const letra = word[i].toLowerCase();
+  const letra = word[i].toLowerCase(); // "A" -> "a"
   if (
     letra === "a" ||
     letra === "e" ||
@@ -21,9 +20,17 @@ for (let i = 0; i < word.length; i++) {
 }
 console.log(`Tu palabra tiene: ${counter} vocales`);
 
+
 // Ejercicio: Verificar si un número es primo usando FOR
+// Le vamos a pedir al usuario , que solocque un numero y el programa nos debe devolver:
+
+// - el numero  XX es primo !
+// - el numero XX no es primo :c.
 // Un número primo solo es divisible por 1 y por sí mismo
 // aqui implementacion
+
+
+
 
 // Ejercicio: Encontrar el número mayor usando FOR
 let numeros = [23, 45, 12, 67, 34, 89, 5, 78];
