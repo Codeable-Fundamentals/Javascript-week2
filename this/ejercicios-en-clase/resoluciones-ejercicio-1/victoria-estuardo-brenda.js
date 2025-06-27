269c7cdca5d9b6444399d99dc9c965dbce9f2cfc
vi18xd <jenni_jc9@hotmail.com>
2025-06-26 19:31:17 -0500

    ejercicio-VEB

---
+++ this/ejercicios-en-clase/resoluciones-ejercicio-1/victoria-estuardo-brenda.js
@@ -0,0 +1,41 @@
+// Ejercicio 1: Crear un Objeto Usuario
+// Descripción: Crea un objeto llamado usuario que contenga información personal y métodos para interactuar con ella.
+// Lo que tienes que hacer:
+
+// Crea un objeto usuario con las propiedades: nombre, email, edad
+// Agrega un método saludar() que imprima: "Hola, soy [nombre] y mi email es [email]"
+// Agrega un método cumplirAnios() que incremente la edad en 1 y muestre: "[nombre] ahora tiene [edad] años"
+// Agrega un método cambiarEmail(nuevoEmail) que actualice el email
+
+// Código base:
+const usuario = {
+    // Completa aquí
+
+    nombre: "Juan",
+    email: "juan@gmail.com",
+    edad: 25,
+
+    saludar: function() {
+        console.log(`Hola, soy ${this.nombre} y mi email es ${this.email}`);
+    },
+
+    cumplirAnios: function() {
+        this.edad++;
+        console.log(`${this.nombre} ahora tiene ${this.edad} años`);
+    },
+
+    cambiarEmail: function(nuevoEmail) {
+        this.email = nuevoEmail;
+    }
+};
+
+// Pruebas
+usuario.saludar();
+usuario.cumplirAnios();
+usuario.cambiarEmail("nuevo@email.com");
+usuario.saludar();
+
+
+// Hola, soy Juan y mi email es juan@email.com
+// Juan ahora tiene 26 años
+// Hola, soy Juan y mi email es nuevo@email.com
