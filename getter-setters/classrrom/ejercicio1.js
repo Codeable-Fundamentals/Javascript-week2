a86888d036f0618becb4cc145c08806e34c94618
Grecia Delgado <greciadm2596@gmail.com>
2025-06-26 19:53:26 -0600

    save ejemplo de getter and setters

--- getter-setters/classrrom/ejercicio1.js
+++ getter-setters/classrrom/ejercicio1.js
@@ -11,9 +11,21 @@ console.log("Caso como metodo (funcion): ", persona.obtenerNombreCompleto());
 const persona2 = {
   nombre: "Simon",
   apellido: "Grau",
+  age: 12,
   get NombreCompleto() {
     return this.nombre + " " + this.apellido + "!";
+  },
+  get ObtenerEdad() {
+    return this.age;
+  },
+  set actualizarEdad(nuevaEdad) {
+    this.age = nuevaEdad;
   },
 };
 
 console.log("Caso como propiedad: ", persona2.NombreCompleto);
+console.log("Caso como propiedad: ", persona2.ObtenerEdad);
+
+persona2.actualizarEdad = 25;
+console.log("Aculizando edad de persona 2 ....");
+console.log("Caso como propiedad: ", persona2.ObtenerEdad);
