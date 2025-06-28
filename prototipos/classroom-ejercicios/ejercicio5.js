28cdc52ea46e7b804bb5507efbe5ba0df6f55fb9
Grecia Delgado <greciadm2596@gmail.com>
2025-06-27 18:57:13 -0600

    new examples

--- prototipos/classroom-ejercicios/ejercicio5.js
+++ prototipos/classroom-ejercicios/ejercicio5.js
@@ -1,18 +1,34 @@
+let pelicula = {
+  esPersonaje: true,
+};
+
+let servivo = {
+  respira: true,
+};
+
 let humano = {
   corazon: true,
 };
 
 let maryJean = {
   pelo: "rojo",
-  __proto__: humano,
+  /* Esto no se puede hacer*/
+  // __proto__: humano,
+  //__proto__: servivo,
 };
 
+maryJean.__proto__ = pelicula;
+// maryJean.__proto__ = humano;
+maryJean.__proto__ = servivo;
 
-console.log(humano.pelo); // undefined
-console.log(maryJean.pelo); // rojo
+console.log("maryJean.respira: ", maryJean.respira); // ??
+console.log("maryJean.esPersonaje: ", maryJean.esPersonaje); // ??
 
-console.log(humano.corazon); // true
-console.log(maryJean.corazon); // true
+console.log("humano.pelo :", humano.pelo); // undefined
+console.log("maryJean.pelo: ", maryJean.pelo); // rojo
 
-console.log(humano.cola); // undefined
-console.log(maryJean.cola); // undefined
+console.log("humano.corazon: ", humano.corazon); // true
+console.log("maryJean.corazon: ", maryJean.corazon); // true
+
+console.log("humano.cola: ", humano.cola); // undefined
+console.log("maryJean.cola: ", maryJean.cola); // undefined
