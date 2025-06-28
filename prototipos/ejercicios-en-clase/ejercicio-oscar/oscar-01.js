dafebe739a1adfd9e511ca9631bc2d5d722c1053
Krascar <oscarmamani0079@gmail.com>
2025-06-27 20:52:23 -0500

    ejercicio-1

---
+++ prototipos/ejercicios-en-clase/ejercicio-oscar/oscar-01.js
@@ -0,0 +1,19 @@
+let animal = {
+  tipo : "''animal'' (heredado)",
+  respirar() {
+    return "''respirando''";
+  }
+}
+let perro = {
+  raza: "''canino''",
+  ladrar: function() {
+    return "''guau guau''";
+  }
+};
+
+perro.__proto__ = animal;
+
+console.log(perro.raza);      // "canino"
+console.log(perro.tipo);      // "animal" (heredado)
+console.log(perro.ladrar());  // "guau guau"
+console.log(perro.respirar());
